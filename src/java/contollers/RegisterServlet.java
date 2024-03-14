dea834eca12e9f4a478b588456ecb76dcf718938
LoveDoLove <v0130p5100cuboss@gmail.com>
2024-03-15 01:08:55 +0800

    added mail services
    - added otp services to register
    - added json utilities for response to jsp jscript
    - added random utilities for generate otp

--- src/java/contollers/RegisterServlet.java
+++ src/java/contollers/RegisterServlet.java
@@ -1,6 +1,5 @@
 package contollers;
 
-import common.Common;
 import common.Constants;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -9,7 +8,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import request.RegisterRequest;
 import response.RegisterResponse;
+import services.MailServices;
 import services.RegisterServices;
+import utilities.JsonUtilities;
 import utilities.RedirectUtilities;
 import utilities.StringUtilities;
 
@@ -32,53 +33,89 @@ public class RegisterServlet extends HttpServlet {
             String path = request.getServletPath();
             switch (path) {
                 case Constants.REGISTER_URL:
-                    registerCustomer(request, response);
+                    if (!registerCustomer(request, response)) {
+
+                        request.getRequestDispatcher(Constants.REGISTER_JSP_URL).forward(request, response);
+
+                    }
+                    return;
+                case Constants.REGISTER_SEND_OTP_URL:
+                    sendOtp(request, response);
                     return;
             }
         }
         request.getRequestDispatcher(Constants.REGISTER_JSP_URL).forward(request, response);
     }
 
-    private void registerCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private boolean registerCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+        String confirm_password = request.getParameter("confirm_password");
         String email = request.getParameter("email");
         String address = request.getParameter("address");
         String phone_number = request.getParameter("phone_number");
         String gender = request.getParameter("gender");
+        String otp = request.getParameter("otp");
 
         request.setAttribute("username", username);
         request.setAttribute("email", email);
         request.setAttribute("address", address);
         request.setAttribute("phone_number", phone_number);
-        request.setAttribute("gender", gender);
 
-        RegisterRequest registerRequest = new RegisterRequest(username, password, email, address, phone_number, gender);
+        RegisterRequest registerRequest = new RegisterRequest(username, password, confirm_password, email, address, phone_number, gender, otp);
 
         if (!validateRegisterRequest(registerRequest)) {
-            RedirectUtilities.redirectWithError(request, response, "Please Fill All Fields Correctly!", Constants.REGISTER_URL);
-            return;
+            RedirectUtilities.setErrorMessage(request, "Please Fill In All The Fields!");
+            return false;
         }
 
-        RegisterResponse registerResponse = registerServices.addNewCustomer(registerRequest);
-
-        if (registerResponse == null || registerResponse.getStatus() == Common.Status.INTERNAL_SERVER_ERROR) {
-            RedirectUtilities.setErrorMessage(request, "Internal Server Error!");
-        } else if (registerResponse.getStatus() == Common.Status.EXISTS) {
-            RedirectUtilities.setErrorMessage(request, "Email Already Exists!");
-        } else if (registerResponse.getStatus() == Common.Status.OK) {
-            RedirectUtilities.setSuccessMessage(request, "Registered Successfully!");
-            RedirectUtilities.sendRedirect(request, response, Constants.LOGIN_URL);
-            return;
+        if (!password.equals(confirm_password)) {
+            RedirectUtilities.setErrorMessage(request, "Password and Confirm Password should be same!");
+            return false;
         }
-        RedirectUtilities.sendRedirect(request, response, Constants.REGISTER_URL);
+
+        RegisterResponse registerResponse = registerServices.getOtp(email);
+
+        switch (registerResponse.getStatus()) {
+            case INTERNAL_SERVER_ERROR:
+                RedirectUtilities.setErrorMessage(request, "Internal Server Error!");
+                return false;
+            case NOT_FOUND:
+                RedirectUtilities.setErrorMessage(request, "Email Not Found!");
+                return false;
+            case OK:
+                if (!otp.equals(registerResponse.getOtp())) {
+                    RedirectUtilities.setErrorMessage(request, "Invalid OTP!");
+                    return false;
+                }
+                break;
+        }
+
+        registerResponse = registerServices.addNewCustomer(registerRequest);
+
+        switch (registerResponse.getStatus()) {
+            case INTERNAL_SERVER_ERROR:
+                RedirectUtilities.setErrorMessage(request, "Internal Server Error!");
+                return false;
+            case EXISTS:
+                RedirectUtilities.setErrorMessage(request, "Email Already Exists!");
+                return false;
+            case OK:
+                RedirectUtilities.setSuccessMessage(request, "Registered Successfully!");
+                sendWelcomeMessage(username, email);
+                RedirectUtilities.sendRedirect(request, response, Constants.LOGIN_URL);
+                return true;
+        }
+
+        RedirectUtilities.setErrorMessage(request, "Failed to Register!");
+        return false;
     }
 
     private boolean validateRegisterRequest(RegisterRequest registerRequest) {
         if (registerRequest == null) {
             return false;
         }
-        if (StringUtilities.anyNullOrBlank(registerRequest.getUsername(), registerRequest.getPassword(), registerRequest.getEmail(), registerRequest.getAddress(), registerRequest.getPhoneNumber(), registerRequest.getGender())) {
+        if (StringUtilities.anyNullOrBlank(registerRequest.getUsername(), registerRequest.getPassword(), registerRequest.getConfirm_password(), registerRequest.getEmail(), registerRequest.getAddress(), registerRequest.getPhoneNumber(), registerRequest.getGender(), registerRequest.getOtp())) {
             return false;
         }
         if (registerRequest.getPassword().length() < 8) {
@@ -90,5 +127,38 @@ public class RegisterServlet extends HttpServlet {
             }
         }
         return true;
+    }
+
+    private void sendOtp(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        if (StringUtilities.anyNullOrBlank(email)) {
+            JsonUtilities.sendErrorResponse(response, "Please fill in the email field!");
+            return;
+        }
+        RegisterResponse registerResponse = registerServices.sendOtp(email);
+        switch (registerResponse.getStatus()) {
+            case INTERNAL_SERVER_ERROR:
+                JsonUtilities.sendErrorResponse(response, "Internal Server Error!");
+                return;
+            case OK:
+                JsonUtilities.sendSuccessResponse(response, "OTP Sent Successfully!");
+                return;
+        }
+        JsonUtilities.sendErrorResponse(response, "Failed to send OTP!");
+    }
+
+    private void sendWelcomeMessage(String username, String email) {
+        String subject = String.format("Welcome to %s %s", username, Constants.COMPANY_NAME);
+        String body = String.format("Hey %s! Welcome to our store!\n"
+                + "\n"
+                + "Thank you for creating a %s account. We are more than happy to have you on board.\n"
+                + "\n"
+                + "Please make yourself enjoy shopping with us.\n"
+                + "\n"
+                + "But before you start, here’s a short tutorial we created for our new clients. It will help you find the best fit based on size, color, and body type.\n"
+                + "\n"
+                + "See you around,\n"
+                + "%s Team", username, Constants.COMPANY_NAME, Constants.COMPANY_NAME);
+        new MailServices().sendEmail(email, subject, body);
     }
 }
--- src/java/services/RegisterServices.java
+++ src/java/services/RegisterServices.java
@@ -1,6 +1,7 @@
 package services;
 
 import common.Common;
+import common.Constants;
 import contollers.ConnectionController;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,11 +10,21 @@ import java.sql.SQLException;
 import request.RegisterRequest;
 import response.RegisterResponse;
 import utilities.AesUtilities;
+import utilities.RandomUtilities;
 
 public class RegisterServices {
 
+    private final String OTP_EMAIL_SUBJECT = Constants.COMPANY_NAME + " OTP";
+    private final String OTP_EMAIL_BODY = "Your OTP is: ";
+
     private final String ADD_NEW_CUSTOMER_SQL = "INSERT INTO customers (username, password, email, address, phone_number, gender) VALUES (?, ?, ?, ?, ?, ?)";
     private final String COUNT_EMAIL_SQL = "SELECT COUNT(*) FROM customers WHERE email = ?";
+
+    private final String GET_OTP_SQL = "SELECT otp FROM otps WHERE email = ?";
+    private final String COUNT_OTP_SQL = "SELECT COUNT(*) FROM otps WHERE email = ?";
+    private final String ADD_OTP_SQL = "INSERT INTO OTPS(email, otp) VALUES(?, ?)";
+    private final String UPDATE_OTP_SQL = "UPDATE otps SET otp = ? WHERE email = ?";
+    private final String DELETE_OTP_SQL = "DELETE FROM otps WHERE email = ?";
 
     public RegisterResponse addNewCustomer(RegisterRequest registerRequest) {
         RegisterResponse registerResponse = new RegisterResponse();
@@ -32,6 +43,9 @@ public class RegisterServices {
                 preparedStatement.setString(6, registerRequest.getGender());
                 int update = preparedStatement.executeUpdate();
                 registerResponse.setStatus(update > 0 ? Common.Status.OK : Common.Status.INTERNAL_SERVER_ERROR);
+                if (registerResponse.getStatus() == Common.Status.OK) {
+                    deleteOtp(connection, registerRequest.getEmail());
+                }
             } catch (SQLException ex) {
                 System.err.println("Error adding new customer: " + ex.getMessage());
                 registerResponse.setStatus(Common.Status.INTERNAL_SERVER_ERROR);
@@ -41,6 +55,82 @@ public class RegisterServices {
             registerResponse.setStatus(Common.Status.INTERNAL_SERVER_ERROR);
         }
         return registerResponse;
+    }
+
+    public RegisterResponse getOtp(String email) {
+        RegisterResponse registerResponse = new RegisterResponse();
+        try (Connection connection = ConnectionController.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(GET_OTP_SQL)) {
+            preparedStatement.setString(1, email);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    registerResponse.setOtp(resultSet.getString("otp"));
+                    registerResponse.setStatus(Common.Status.OK);
+                } else {
+                    registerResponse.setStatus(Common.Status.NOT_FOUND);
+                }
+            }
+        } catch (SQLException ex) {
+            System.err.println("Error establishing database connection: " + ex.getMessage());
+            registerResponse.setStatus(Common.Status.INTERNAL_SERVER_ERROR);
+        }
+        return registerResponse;
+    }
+
+    public RegisterResponse sendOtp(String email) {
+        RegisterResponse registerResponse = new RegisterResponse();
+        String otp = RandomUtilities.generateOtp();
+        try (Connection connection = ConnectionController.getConnection()) {
+            Boolean otpExists = isOtpExists(connection, email);
+            if (otpExists == null || (otpExists ? updateOtp(connection, email, otp) : addOtp(connection, email, otp))) {
+                registerResponse.setStatus(Common.Status.OK);
+                new MailServices().sendEmail(email, OTP_EMAIL_SUBJECT, OTP_EMAIL_BODY + otp);
+            } else {
+                registerResponse.setStatus(Common.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (SQLException ex) {
+            System.err.println("Error establishing database connection: " + ex.getMessage());
+            registerResponse.setStatus(Common.Status.INTERNAL_SERVER_ERROR);
+        }
+        return registerResponse;
+    }
+
+    private boolean addOtp(Connection connection, String email, String otp) {
+        return executeUpdate(connection, ADD_OTP_SQL, email, otp);
+    }
+
+    private boolean updateOtp(Connection connection, String email, String otp) {
+        return executeUpdate(connection, UPDATE_OTP_SQL, otp, email);
+    }
+
+    private boolean deleteOtp(Connection connection, String email) {
+        return executeUpdate(connection, DELETE_OTP_SQL, email, null);
+    }
+
+    private Boolean isOtpExists(Connection connection, String email) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(COUNT_OTP_SQL)) {
+            preparedStatement.setString(1, email);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt(1) > 0;
+                }
+            }
+        } catch (SQLException ex) {
+            System.err.println("Error checking if OTP exists: " + ex.getMessage());
+            return null;
+        }
+        return false;
+    }
+
+    private boolean executeUpdate(Connection connection, String sql, String param1, String param2) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, param1);
+            preparedStatement.setString(2, param2);
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException ex) {
+            System.err.println("Error executing update: " + ex.getMessage());
+            return false;
+        }
     }
 
     private boolean isEmailExists(Connection connection, String email) {
