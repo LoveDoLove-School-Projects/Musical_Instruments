7c86425da3c18160887b76ebfd002bdd382e343a
LoveDoLove <v0130p5100cuboss@gmail.com>
2024-03-29 13:48:45 +0800

    update register business logics

--- src/java/controllers/RegisterServlet.java
+++ src/java/controllers/RegisterServlet.java
@@ -4,7 +4,6 @@ import domain.common.Common;
 import domain.common.Constants;
 import domain.models.Session;
 import domain.request.RegisterRequest;
-import domain.response.RegisterResponse;
 import features.SessionHandler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -18,25 +17,31 @@ import utilities.StringUtilities;
 import utilities.enums.RedirectType;
 
 public class RegisterServlet extends HttpServlet {
-
-    private static final RegisterServices registerServices = new RegisterServices();
-    private static final OtpServices otpServices = new OtpServices();
-    private static final SessionHandler sessionHandler = new SessionHandler();
-
+    
+    private final RegisterServices registerServices;
+    private final OtpServices otpServices;
+    private final SessionHandler sessionHandler;
+    
+    public RegisterServlet() {
+        this.registerServices = new RegisterServices();
+        this.otpServices = new OtpServices();
+        this.sessionHandler = new SessionHandler();
+    }
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         handleRegister(request, response);
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         handleRegister(request, response);
     }
-
+    
     private void setRegisterPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher(Constants.REGISTER_JSP_URL).forward(request, response);
     }
-
+    
     private void handleRegister(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Session session = sessionHandler.getLoginSession(request, response);
         if (session.isResult()) {
@@ -45,20 +50,48 @@ public class RegisterServlet extends HttpServlet {
         }
         String path = request.getServletPath();
         if ("POST".equalsIgnoreCase(request.getMethod())) {
-            boolean isRegistered = false;
             switch (path) {
                 case Constants.REGISTER_URL:
-                    isRegistered = registerCustomer(request, response);
-                    break;
-            }
-            if (isRegistered) {
-                return;
+                    Common.Status registerStatus = addNewCustomer(request, response);
+                    handleRegisterStatus(registerStatus, request, response);
+                    return;
             }
         }
         setRegisterPage(request, response);
     }
-
-    private boolean registerCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    private Common.Status addNewCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RegisterRequest registerRequest = createRegisterRequest(request);
+        
+        if (!validateRegisterRequest(registerRequest)) {
+            RedirectUtilities.setMessage(request, RedirectType.DANGER, "Please Fill In All The Fields!");
+            return Common.Status.INVALID;
+        }
+        
+        if (!registerRequest.getPassword().equals(registerRequest.getConfirm_password())) {
+            RedirectUtilities.setMessage(request, RedirectType.DANGER, "Password and Confirm Password should be same!");
+            return Common.Status.INVALID;
+        }
+        
+        return registerServices.addNewCustomer(registerRequest);
+    }
+    
+    private void handleRegisterStatus(Common.Status registerStatus, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        switch (registerStatus) {
+            case OK:
+                RedirectUtilities.redirectWithMessage(request, response, RedirectType.SUCCESS, "Registered Successfully!", Constants.CUSTOMER_LOGIN_URL);
+                return;
+            case EXISTS:
+                RedirectUtilities.setMessage(request, RedirectType.DANGER, "Email Already Exists!");
+                break;
+            default:
+                RedirectUtilities.setMessage(request, RedirectType.DANGER, "Failed to Register!");
+                break;
+        }
+        setRegisterPage(request, response);
+    }
+    
+    private RegisterRequest createRegisterRequest(HttpServletRequest request) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String confirm_password = request.getParameter("confirm_password");
@@ -66,47 +99,16 @@ public class RegisterServlet extends HttpServlet {
         String address = request.getParameter("address");
         String phone_number = request.getParameter("phone_number");
         String gender = request.getParameter("gender");
-
+        
         request.setAttribute("username", username);
         request.setAttribute("email", email);
         request.setAttribute("address", address);
         request.setAttribute("phone_number", phone_number);
         request.setAttribute("gender", gender);
-
-        RegisterRequest registerRequest = new RegisterRequest(username, password, confirm_password, email, address, phone_number, gender);
-
-        if (!validateRegisterRequest(registerRequest)) {
-            RedirectUtilities.setMessage(request, RedirectType.DANGER, "Please Fill In All The Fields!");
-            return false;
-        }
-
-        if (!password.equals(confirm_password)) {
-            RedirectUtilities.setMessage(request, RedirectType.DANGER, "Password and Confirm Password should be same!");
-            return false;
-        }
-
-        RegisterResponse registerResponse = registerServices.addNewCustomer(registerRequest);
-
-        if (registerResponse.getStatus() == null) {
-            RedirectUtilities.setMessage(request, RedirectType.DANGER, "Failed to Register!");
-            return false;
-        }
-
-        if (registerResponse.getStatus() == Common.Status.EXISTS) {
-            RedirectUtilities.setMessage(request, RedirectType.DANGER, "Email Already Exists!");
-            return false;
-        }
-
-        if (registerResponse.getStatus() == Common.Status.OK) {
-            otpServices.deleteOtp(email);
-            RedirectUtilities.redirectWithMessage(request, response, RedirectType.SUCCESS, "Registered Successfully!", Constants.CUSTOMER_LOGIN_URL);
-            return true;
-        }
-
-        RedirectUtilities.setMessage(request, RedirectType.DANGER, "Failed to Register!");
-        return false;
+        
+        return new RegisterRequest(username, password, confirm_password, email, address, phone_number, gender);
     }
-
+    
     private boolean validateRegisterRequest(RegisterRequest registerRequest) {
         if (registerRequest == null) {
             return false;
