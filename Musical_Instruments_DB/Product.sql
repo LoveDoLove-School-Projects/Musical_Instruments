de1760084b2cbb458960e586acf6cde5018bc532
Kaiquann <130069327+Kaiquann@users.noreply.github.com>
2024-04-07 15:12:41 +0800

    update image id (#54)

---
+++ Musical_Instruments_DB/Product.sql
@@ -0,0 +1,11 @@
+CREATE TABLE products (
+    product_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    color VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    image_path VARCHAR(255) NOT NULL
+);
+
+DROP TABLE products;
