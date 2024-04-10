35a956f5365c84635e7aa69e78d412db07f055b2
LoveDoLove <67772009+LoveDoLove@users.noreply.github.com>
2024-04-10 20:05:19 +0800

    rearrange common constants (#59)

--- src/java/domain/common/Constants.java
+++ src/java/domain/common/Constants.java
@@ -2,43 +2,49 @@ package domain.common;
 
 public final class Constants {
 
+    /* ********************************* */
+    //    Common Constants
     public static final String DATE_PATTERN = "yyyy-MM-dd";
     public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
     public static final String COMPANY_NAME = "Musical Instruments";
-    //    JSP URL
+    /* ********************************* */
+ /* ********************************* */
+    //    MAIN JSP URL
     public static final String MAIN_JSP_URL = "/pages/main.jsp";
-    public static final String LOGIN_JSP_URL = "/pages/login.jsp";
-    public static final String REGISTER_JSP_URL = "/pages/register.jsp";
-    public static final String PROFILE_JSP_URL = "/pages/profile.jsp";
-
-    //    Product JSP URL
-    public static final String PRODUCT_JSP_URL = "/pages/product.jsp";
-    public static final String VIEW_PRODUCT_JSP_URL = "/pages/viewProduct.jsp";
-
-    //    Servlet URL
+    //    MAIN URL
     public static final String MAIN_URL = "/pages/main";
+    /* ********************************* */
+ /* ********************************* */
+    //    REGISTER JSP URL
+    public static final String REGISTER_JSP_URL = "/pages/register.jsp";
+    //    REGISTER URL
     public static final String REGISTER_URL = "/pages/register";
-    public static final String PROFILE_URL = "/pages/profile";
-    public static final String PRODUCT_URL = "/pages/product";
-
+    /* ********************************* */
+ /* ********************************* */
+    //    LOGIN JSP URL
+    public static final String LOGIN_JSP_URL = "/pages/login.jsp";
     //    Login URL
     public static final String CUSTOMER_LOGIN_URL = "/pages/login";
     public static final String ADMIN_LOGIN_URL = "/pages/adminLogin";
-
-    //    Profile Method URL
+    /* ********************************* */
+ /* ********************************* */
+    //    Profile JSP URL
+    public static final String PROFILE_JSP_URL = "/pages/profile.jsp";
+    //    Profile URL
+    public static final String PROFILE_URL = "/pages/profile";
     public static final String PROFILE_UPLOAD_PICTURE_URL = "/pages/profile/uploadPicture";
     public static final String PROFILE_REMOVE_PICTURE_URL = "/pages/profile/removePicture";
     public static final String PROFILE_UPDATE_URL = "/pages/profile/updateProfile";
-
-    //    Session JSP URL
-    public static final String LOGIN_2FA_JSP_URL = "/sessions/login2fa.jsp";
-
-    //    Session URL
-    public static final String LOGIN_2FA_URL = "/sessions/login2fa";
-
+    /* ********************************* */
+ /* ********************************* */
+    //    Product JSP URL
+    public static final String PRODUCT_JSP_URL = "/pages/product.jsp";
+    public static final String VIEW_PRODUCT_JSP_URL = "/pages/viewProduct.jsp";
     //    Product URL
+    public static final String PRODUCT_URL = "/pages/product";
     public static final String VIEW_PRODUCT_URL = "/pages/product/viewProduct";
-
+    /* ********************************* */
+ /* ********************************* */
     //    Admin JSP URL
     public static final String ADMIN_JSP_URL = "/pages/admins/admin.jsp";
     public static final String ADMIN_CONTROL_PANEL_JSP_URL = "/pages/admins/controlPanel.jsp";
@@ -47,7 +53,6 @@ public final class Constants {
     public static final String ADMIN_MANAGE_STOCK_JSP_URL = "/pages/admins/manageStock.jsp";
     public static final String ADMIN_SALES_JSP_URL = "/pages/admins/sales.jsp";
     public static final String ADMIN_VIEW_TRANSACTION_JSP_URL = "/pages/admins/viewTransaction.jsp";
-
     //    Admin URL
     public static final String ADMIN_URL = "/pages/admin";
     public static final String ADMIN_CONTROL_PANEL_URL = "/pages/admin/controlPanel";
@@ -56,4 +61,19 @@ public final class Constants {
     public static final String ADMIN_MANAGE_STOCK_URL = "/pages/admin/manageStock";
     public static final String ADMIN_SALES_URL = "/pages/admin/sales";
     public static final String ADMIN_VIEW_TRANSACTION_URL = "/pages/admin/viewTransaction";
+    /* ********************************* */
+ /* ********************************* */
+    //    Transaction JSP URL
+    public static final String TRANSACTION_JSP_URL = "/payments/transaction.jsp";
+    //    Transaction URL
+    public static final String TRANSACTION_URL = "/payments/transaction";
+    public static final String DO_TRANSACTION_URL = "/payments/doTransaction";
+
+    /* ********************************* */
+ /* ********************************* */
+    //    Session JSP URL
+    public static final String LOGIN_2FA_JSP_URL = "/sessions/login2fa.jsp";
+    //    Session URL
+    public static final String LOGIN_2FA_URL = "/sessions/login2fa";
+    /* ********************************* */
 }
