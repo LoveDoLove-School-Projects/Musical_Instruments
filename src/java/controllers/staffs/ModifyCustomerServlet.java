ac4c9f3fe2c495b8b8515affaa436521a9672005
LoveDoLove <v0130p5100cuboss@gmail.com>
2024-04-26 17:00:12 +0800

    fixed admin update  and manage customer bugs

--- src/java/controllers/staffs/ModifyCustomerServlet.java
+++ src/java/controllers/staffs/ModifyCustomerServlet.java
@@ -8,23 +8,20 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.HeuristicMixedException;
+import jakarta.transaction.HeuristicRollbackException;
+import jakarta.transaction.NotSupportedException;
+import jakarta.transaction.RollbackException;
+import jakarta.transaction.SystemException;
 import jakarta.transaction.UserTransaction;
 import java.io.IOException;
-import java.util.logging.Logger;
-import services.StaffServices;
 import utilities.RedirectUtilities;
 
 public class ModifyCustomerServlet extends HttpServlet {
 
-    private static final Logger LOG = Logger.getLogger(ModifyCustomerServlet.class.getName());
-
-    private final StaffServices staffServices = new StaffServices();
     private static final String SHOW_CUSTOMER_DETAILS_URL = "/pages/staffs/manageCustomer";
-
     @PersistenceContext
     EntityManager entityManager;
-
     @Resource
     UserTransaction userTransaction;
 
@@ -36,23 +33,41 @@ public class ModifyCustomerServlet extends HttpServlet {
         String gender = request.getParameter("gender");
         String address = request.getParameter("address");
         String phoneNumber = request.getParameter("phoneNumber");
-
         if (userIdString == null || userIdString.isEmpty()) {
-            RedirectUtilities.redirectWithMessage(request, response, RedirectUtilities.RedirectType.DANGER, "Invalid detail, please reenter it.", "/pages/staffs/modifyCustomer.jsp");
+            showError(request, response);
             return;
         }
         int userId = Integer.parseInt(userIdString);
         Customers customer = new Customers(userId, username, address, phoneNumber, gender);
-
-        boolean isUpdated = staffServices.modifyCustomerDetailsDB(customer);
+        boolean isUpdated = updateCustomerDetails(customer);
         if (!isUpdated) {
-            RedirectUtilities.redirectWithMessage(request, response, RedirectUtilities.RedirectType.DANGER, "Invalid detail, please reenter it.", "/pages/staffs/modifyCustomer.jsp");
+            showError(request, response);
             return;
         }
-        Customers customersList = staffServices.searchCustomerDetailsDB(userId);
-        HttpSession session = request.getSession();
-        session.setAttribute("customersList", customersList);
-        LOG.info(customersList.toString());
         RedirectUtilities.redirectWithMessage(request, response, RedirectUtilities.RedirectType.SUCCESS, "Update Successful!", SHOW_CUSTOMER_DETAILS_URL);
+    }
+
+    private boolean updateCustomerDetails(Customers customer) {
+        try {
+            userTransaction.begin();
+            Customers customerFromDB = entityManager.find(Customers.class, customer.getUserId());
+            if (customerFromDB == null) {
+                return false;
+            }
+            customerFromDB.setAddress(customer.getAddress());
+            customerFromDB.setGender(customer.getGender());
+            customerFromDB.setPhoneNumber(customer.getPhoneNumber());
+            customerFromDB.setUsername(customer.getUsername());
+            entityManager.merge(customerFromDB);
+            userTransaction.commit();
+            return true;
+        } catch (HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException | IllegalStateException | SecurityException e) {
+            return false;
+        }
+    }
+
+    private void showError(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RedirectUtilities.redirectWithMessage(request, response, RedirectUtilities.RedirectType.DANGER, "Invalid details, please re-enter it.", "/pages/staffs/modifyCustomer.jsp");
     }
 }
