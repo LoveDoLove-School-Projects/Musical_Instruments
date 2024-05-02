5baf072484fc9400170c11da24b14f849e06ee8f
kaiquan <kaiquan3011@gmail.com>
2024-05-03 02:34:22 +0800

    Reapply "set up order page and improve some design"
    This reverts commit cb30465b2d83ef7082c9932cd4f13fd75f6d098e.

---
+++ src/java/controllers/orders/updateOrder.java
@@ -0,0 +1,27 @@
+
+package controllers.orders;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class updateOrder extends HttpServlet {
+
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      
+    }
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+   
+}
