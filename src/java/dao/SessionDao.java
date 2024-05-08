9d10ce10eb8dc1be057a6f8f9128bb5f510d4904
LoveDoLove <v0130p5100cuboss@gmail.com>
2024-05-08 11:41:14 +0800

    fix session checker (#155)

--- src/java/dao/SessionDao.java
+++ src/java/dao/SessionDao.java
@@ -7,25 +7,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SessionDao {
 
     private static final String CHECK_SESSION_SQL = "SELECT * FROM table_name WHERE user_id = ?";
-    private final Map<String, Boolean> cache = new HashMap<>();
 
     public boolean checkSessionIsExistOrNot(Session session) throws DatabaseException {
-        String key = session.getRole().getRole() + "-" + session.getUserId();
-        if (cache.containsKey(key)) {
-            return cache.get(key);
-        }
         try (Connection connection = ConnectionController.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(replaceQuery(session.getRole().getRole()))) {
             preparedStatement.setInt(1, session.getUserId());
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                boolean result = resultSet.next();
-                cache.put(key, result);
-                return result;
+                return resultSet.next();
             }
         } catch (SQLException ex) {
             throw new DatabaseException(ex.getMessage());
